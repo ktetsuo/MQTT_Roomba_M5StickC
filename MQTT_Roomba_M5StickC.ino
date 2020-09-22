dabb5b097ea1162bfd511c21a103586f089c579b
ktetsuo <ktetsuo@users.noreply.github.com>
2020-09-22 23:22:04 +0900

    initial commit

---
+++ MQTT_Roomba_M5StickC.ino
@@ -0,0 +1,318 @@
+#include <M5StickC.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+
+#include "Roomba.h"
+
+// WiFi 設定
+const char ssid[] = "";
+const char password[] = "";
+WiFiClient wifiClient;
+
+// MQTT 設定
+const char* mqttBrokerAddr = "tailor.cloudmqtt.com";
+const char* mqttUserName = "";
+const char* mqttPassword = "";
+const int mqttPort = 11333;
+const char* mqttClientID = "Roomba";
+PubSubClient mqttClient(mqttBrokerAddr, mqttPort, wifiClient);
+
+// Roombaインスタンス
+Roomba roomba(Serial1, 100);
+
+// グローバル変数
+float accX = 0.0F;
+float accY = 0.0F;
+float accZ = 0.0F;
+float gyroX = 0.0F;
+float gyroY = 0.0F;
+float gyroZ = 0.0F;
+float pitch = 0.0F;
+float roll  = 0.0F;
+float yaw   = 0.0F;
+boolean sending = false;
+unsigned long lastUpdateTime = 0;
+int lastWiFiStatus;
+
+void setup() {
+  M5.begin();
+  M5.IMU.Init();
+  M5.Lcd.setRotation(3);
+  M5.Lcd.fillScreen(BLACK);
+  M5.Lcd.setTextSize(1);
+  M5.Lcd.setCursor(0, 20);
+  M5.Lcd.println("  X       Y       Z");
+  M5.Lcd.setCursor(0, 50);
+  M5.Lcd.println("  Pitch   Roll    Yaw");
+  Serial.begin(115200);
+  Serial1.begin(115200, SERIAL_8N1, 0, 26); // EXT_IO
+  roomba.start();
+  WiFi.begin(ssid, password);
+  WiFi.begin();
+  lastWiFiStatus = WiFi.status();
+  Serial.println();
+  mqttClient.setCallback(mqttCallback);
+}
+
+void loop() {
+  unsigned long t = millis();
+  M5.update();
+  checkWifi();
+  checkMQTT();
+  // 500msごとに更新
+  if (t - lastUpdateTime >= 500) {
+    lastUpdateTime = t;
+    readSensor(); // センサー読み取り
+    updateLcd();  // LCD更新
+    if (sending) {
+      sendSensorData(); // センサーデータ送信
+    }
+  }
+  // ボタンBが押されたら送信の有効無効を切り替える
+  if (M5.BtnB.wasPressed()) {
+    sending = !sending;
+  }
+  static unsigned long pressms = 0;
+  // ボタンAが押されたら
+  if (M5.BtnA.wasPressed()) {
+    // 即送信する
+    mqttClient.publish("roomba/out/ButtonA", "Pressed");
+    roomba.start();
+    pressms = millis();
+  }
+  // ボタンAが離されたら
+  if (M5.BtnA.wasReleased()) {
+    // 即送信する
+    mqttClient.publish("roomba/out/ButtonA", "Released");
+    if(millis() - pressms >= 2000) {
+      roomba.stop(); // 長押しで操作モード終了
+    } else {
+      roomba.full(); // 単押しでフルコントロールモード
+    }
+  }
+  if (Serial.available() > 0) {
+    int c = Serial.read();
+    switch (c) {
+      case '1':
+        Serial.println("Start");
+        roomba.start();
+        break;
+      case '2':
+        Serial.println("Reset");
+        roomba.reset();
+        break;
+      case '3':
+        Serial.println("Stop");
+        roomba.stop();
+        break;
+      case '4':
+        Serial.println("Safe");
+        roomba.safe();
+        break;
+      case '5':
+        Serial.println("Full");
+        roomba.full();
+        break;
+      case '6': // motor forward
+        Serial.println("motor forward");
+        roomba.driveDirect(200, 200);
+        break;
+      case '7': // motor stop
+        Serial.println("motor stop");
+        roomba.driveDirect(0, 0);
+        break;
+      case '8': // motor back
+        Serial.println("motor back");
+        roomba.driveDirect(-200, -200);
+        break;
+      case '9': // get bumper sensors
+        //        Serial.println("get bumper sensors");
+        //        roomba.getBumperSensors();
+        break;
+      default:
+        break;
+    }
+  }
+  static unsigned long lastsensorms = 0;
+  if (t - lastsensorms >= 200) {
+    lastsensorms = t;
+    byte oimode = roomba.getOIMode();
+    if (!roomba.isTimeoutError()) {
+      Serial.print("oimode: ");
+      Serial.println(oimode);
+    }
+    static byte lastBumps = 0;
+    static byte lastDrops = 0;
+    byte bumpsdrops = roomba.getBumpsAndWheelDrops();
+    if (!roomba.isTimeoutError()) {
+      byte bumps = bumpsdrops & Roomba::BUMP_MASK;
+      byte drops = (bumpsdrops & Roomba::DROP_MASK) >> 2;
+      Serial.print("bumps: ");
+      Serial.println(bumps);
+      Serial.print("drops: ");
+      Serial.println(drops);
+      if (bumps != lastBumps) {
+        lastBumps = bumps;
+        mqttClient.publish("roomba/out/bumps", String(bumps).c_str());
+      }
+      if (drops != lastDrops) {
+        lastDrops = drops;
+        mqttClient.publish("roomba/out/drops", String(drops).c_str());
+      }
+    }
+  }
+  if (Serial1.available() > 0) {
+    int c = Serial1.read();
+    Serial.println(c);
+  }
+}
+
+void readSensor() {
+  M5.IMU.getGyroData(&gyroX, &gyroY, &gyroZ);
+  M5.IMU.getAccelData(&accX, &accY, &accZ);
+  M5.IMU.getAhrsData(&pitch, &roll, &yaw);
+}
+void sendSensorData() {
+  mqttClient.publish("roomba/out/AccX", String(accX).c_str());
+  mqttClient.publish("roomba/out/AccY", String(accY).c_str());
+  mqttClient.publish("roomba/out/AccZ", String(accZ).c_str());
+  mqttClient.publish("roomba/out/GyroX", String(gyroX).c_str());
+  mqttClient.publish("roomba/out/GyroY", String(gyroY).c_str());
+  mqttClient.publish("roomba/out/GyroZ", String(gyroZ).c_str());
+  mqttClient.publish("roomba/out/Pitch", String(pitch).c_str());
+  mqttClient.publish("roomba/out/Roll", String(roll).c_str());
+  mqttClient.publish("roomba/out/Yow", String(yaw).c_str());
+}
+
+void updateLcd() {
+  M5.Lcd.setCursor(0, 0);
+  M5.Lcd.print(sending ? "Sending" : "       ");
+  M5.Lcd.setCursor(0, 30);
+  M5.Lcd.printf("%6.2f  %6.2f  %6.2f      ", gyroX, gyroY, gyroZ);
+  M5.Lcd.setCursor(140, 30);
+  M5.Lcd.print("o/s");
+  M5.Lcd.setCursor(0, 40);
+  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", accX, accY, accZ);
+  M5.Lcd.setCursor(140, 40);
+  M5.Lcd.print("G");
+  M5.Lcd.setCursor(0, 60);
+  M5.Lcd.printf(" %5.2f   %5.2f   %5.2f   ", pitch, roll, yaw);
+  M5.Lcd.setCursor(0, 70);
+  M5.Lcd.print("               ");
+  M5.Lcd.setCursor(0, 70);
+  if (WiFi.status() == WL_CONNECTED) {
+    M5.Lcd.print(WiFi.localIP());
+  } else {
+    M5.Lcd.print("WiFi:Disconnect");
+  }
+  M5.Lcd.setCursor(100, 70);
+  M5.Lcd.print(mqttClient.connected() ? "MQTT:Conn" : "MQTT:Disc");
+}
+
+void checkWifi() {
+  int wifiStatus = WiFi.status();
+  if (lastWiFiStatus != WL_CONNECTED && wifiStatus == WL_CONNECTED) {
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+  } else if (lastWiFiStatus == WL_CONNECTED && wifiStatus != WL_CONNECTED) {
+    Serial.println("WiFi disconnected.");
+  }
+  lastWiFiStatus = wifiStatus;
+}
+
+void checkMQTT() {
+  // 接続が切れた際に再接続する
+  static unsigned long lastFailedTime = 0;
+  static boolean lastConnect = false;
+  unsigned long t = millis();
+  if (!mqttClient.connected()) {
+    if (lastConnect || t - lastFailedTime >= 5000) {
+      if (mqttClient.connect(mqttClientID, mqttUserName, mqttPassword)) {
+        Serial.println("MQTT Connect OK.");
+        lastConnect = true;
+        mqttClient.subscribe("roomba/in/#");
+      } else {
+        Serial.print("MQTT Connect failed, rc=");
+        // http://pubsubclient.knolleary.net/api.html#state に state 一覧が書いてある
+        Serial.println(mqttClient.state());
+        lastConnect = false;
+        lastFailedTime = t;
+      }
+    }
+  }
+  mqttClient.loop();
+}
+String payloadToString(const byte* payload, unsigned int length) {
+  char s[length + 1];
+  strncpy(s, (char*)payload, length);
+  s[length] = '\0';
+  return String(s);
+}
+void mqttCallback(char* topic, byte* payload, unsigned int length) {
+  // MQTTトピックが来たときに呼ばれる
+  Serial.print("Message arrived [");
+  Serial.print(topic);
+  Serial.print("] ");
+  String p = payloadToString(payload, length);
+  Serial.println(p);
+  String t(topic);
+  if (!t.startsWith("roomba/in/")) {
+    return;
+  }
+  t.remove(0, strlen("roomba/in/"));
+  Serial.println(t);
+  if (t.equals("drive")) {
+    // payload:コンマ区切り 例: 100,-100
+    int i = p.indexOf(",");
+    String strL = p.substring(0, i);
+    String strR = p.substring(i + 1);
+    int l = strL.toInt();
+    int r = strR.toInt();
+    Serial.println(String("L: ") + String(l) + String(", R: ") + String(r));
+    roomba.full();
+    roomba.driveDirect(l, r);
+  }
+  if (t.equals("mainbrush")) {
+    if (p.equals("on")) {
+      roomba.full();
+      roomba.mainBrushOn();
+    } else if (p.equals("off")) {
+      roomba.full();
+      roomba.mainBrushOff();
+    }
+  }
+  if (t.equals("sidebrush")) {
+    if (p.equals("on")) {
+      roomba.full();
+      roomba.sideBrushOn();
+    } else if (p.equals("off")) {
+      roomba.full();
+      roomba.sideBrushOff();
+    }
+  }
+  if (t.equals("vacuum")) {
+    if (p.equals("on")) {
+      roomba.full();
+      roomba.vacuumOn();
+    } else if (p.equals("off")) {
+      roomba.full();
+      roomba.vacuumOff();
+    }
+  }
+  if (t.equals("command")) {
+    if (p.equals("clean")) {
+      roomba.clean();
+    }
+    if (p.equals("dock")) {
+      roomba.seekDock();
+    }
+    if (p.equals("start")) {
+      roomba.start();
+    }
+    if (p.equals("stop")) {
+      roomba.full(); // CleanやSeekDockを止めるため
+      roomba.stop();
+    }
+  }
+}
---
+++ Roomba.h
@@ -0,0 +1,191 @@
+class Roomba {
+    Stream& _stream;
+    const unsigned long _timeouttime;
+    boolean _timeouterror;
+    byte _motors;
+  public:
+    Roomba(Stream& stream, unsigned long timeouttime)
+      : _stream(stream), _timeouttime(timeouttime), _timeouterror(false), _motors(0)
+    {
+    }
+    boolean isTimeoutError() const {
+      return _timeouterror;
+    }
+    void start() {
+      _stream.write(128); // Start
+    }
+    void reset() {
+      _stream.write(7); // Reset
+    }
+    void stop() {
+      _stream.write(173); // Stop
+    }
+    void safe() {
+      _stream.write(131); // Safe
+    }
+    void full() {
+      _stream.write(132); // Full
+    }
+    void clean() {
+      _stream.write(135); // Clean
+    }
+    void cleanMax() {
+      _stream.write(136); // Max
+    }
+    void spot() {
+      _stream.write(134); // Spot
+    }
+    void seekDock() {
+      _stream.write(143); // Seek Dock
+    }
+    void power() {
+      _stream.write(133); // Power
+    }
+    void sideBrushOn() {
+      _stream.write(138); // Motor
+      _motors |= 0x01;
+      _stream.write(_motors);
+    }
+    void sideBrushOff() {
+      _stream.write(138); // Motor
+      _motors &= ~0x01;
+      _stream.write(_motors);
+    }
+    void mainBrushOn() {
+      _stream.write(138); // Motor
+      _motors |= 0x04;
+      _stream.write(_motors);
+    }
+    void mainBrushOff() {
+      _stream.write(138); // Motor
+      _motors &= ~0x04;
+      _stream.write(_motors);
+    }
+    void vacuumOn() {
+      _stream.write(138); // Motor
+      _motors |= 0x02;
+      _stream.write(_motors);
+    }
+    void vacuumOff() {
+      _stream.write(138); // Motor
+      _motors &= ~0x02;
+      _stream.write(_motors);
+    }
+    void driveDirect(int16_t left, int16_t right) {
+      _stream.write(145); // Drive Direct
+      _stream.write((right >> 8) & 0xff); // Right High
+      _stream.write(right & 0xff); // Right Low
+      _stream.write((left >> 8) & 0xff); // Left High
+      _stream.write(left & 0xff); // Left Low
+    }
+    struct LightBumperSensors {
+      uint16_t left;
+      uint16_t frontleft;
+      uint16_t centerleft;
+      uint16_t centerright;
+      uint16_t frontright;
+      uint16_t right;
+    };
+    LightBumperSensors getLightBumperSensors() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(106); // Group 106(46-51)
+      LightBumperSensors sensors;
+      sensors.left = readUINT16();
+      sensors.frontleft = readUINT16();
+      sensors.centerleft = readUINT16();
+      sensors.centerright = readUINT16();
+      sensors.frontright = readUINT16();
+      sensors.right = readUINT16();
+      return sensors;
+    }
+    byte getBumper() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(45); // Light Bumper
+      return readUINT8();
+    }
+    enum OIMode {
+      OI_OFF = 0,
+      OI_PASSIVE = 1,
+      OI_SAFE = 2,
+      OI_FILL = 3,
+    };
+    byte getOIMode() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(35); // OI Mode
+      return readUINT8();
+    }
+    unsigned int getWallSignal() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(27); // Wall Signal
+      return readUINT16();
+    }
+    enum BumpsAndWheelsBitMask {
+      BUMP_R_BIT = 0x01,
+      BUMP_L_BIT = 0x02,
+      BUMP_MASK = BUMP_R_BIT | BUMP_L_BIT,
+      DROP_R_BIT = 0x04,
+      DROP_L_BIT = 0x08,
+      DROP_MASK = DROP_R_BIT | DROP_L_BIT,
+    };
+    byte getBumpsAndWheelDrops() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(7); // Bumps and Wheel Drops
+      return readUINT8();
+    }
+    enum ChargingState {
+      BATTERY_NOT_CHARGING = 0,
+      BATTERY_RECONDITIONING_CHARGING = 1,
+      BATTERY_FULL_CHARGING = 2,
+      BATTERY_TRICLE_CHARGING = 3,
+      BATTERY_WAITING = 4,
+      BATTERY_CHARGING_FAULT_CONDITION = 5,
+    };
+    ChargingState getChargingState() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(21); // Charging State
+      return (ChargingState)readUINT8();
+    }
+    uint16_t getVoltage() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(22); // Voltage
+      return readUINT16();
+    }
+    uint16_t getCurrent() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(23); // Current
+      return readUINT16();
+    }
+    boolean getChargerAvailable() {
+      _timeouterror = false;
+      _stream.write(142); // Sensor
+      _stream.write(22); // Charging Sources Available
+      return readUINT8() != 0;
+    }
+  private:
+    uint16_t readUINT16() {
+      uint16_t h = readUINT8();
+      uint16_t l = readUINT8();
+      return (h << 8) | l;
+    }
+    uint8_t readUINT8() {
+      if (_timeouterror) {
+        return 0;
+      }
+      unsigned long t0 = millis();
+      while (_stream.available() <= 0) {
+        if (millis() - t0 >= _timeouttime) {
+          _timeouterror = true;
+          return 0;
+        }
+      }
+      return _stream.read();
+    }
+};
